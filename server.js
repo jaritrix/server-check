3a30b3ce88e9c1a1ef2bd12e57607a72ce4cd01c
jaritrix <harshitkumar8057208618@gmail.com>
2025-06-11 12:42:38 +0530

    Update server.js 6th

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const path = require('path');
 
