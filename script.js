a7c94f60b1a1f1ce676785997d305da4a17df9db
jaritrix <harshitkumar8057208618@gmail.com>
2025-06-11 13:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         try {
-        const response = await fetch('http://localhost:3000/signup', {
-, {
+        const response = await fetch('http://localhost:3000/signup' {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData)
@@ -66,8 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         try {
-       const response = await fetch('http://localhost:3000/login', {
-, {
+       const response = await fetch('http://localhost:3000/login'{
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData)
